25d7774c74ee43fad7d537bfecc0cd7214876247
Greg <greg@mac.home>
2025-11-28 21:37:59 +0100

    more  Eloquent queries

--- routes/web.php
+++ routes/web.php
@@ -15,92 +15,62 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
 
 
-    // select * from `users` where `users`.`id` = 1 limit 1
+    // select * from `comments` 
+    // where ((`comments`.`commentable_type` = 'App\\Models\\Image' and exists 
+    // (select * from `images` where `comments`.`commentable_id` = `images`.`id`)) 
+    // or (`comments`.`commentable_type` = 'App\\Models\\Room' and exists (select * from `rooms` 
+    // where `comments`.`commentable_id` = `rooms`.`id`))) and `comments`.`deleted_at` is null
+    // Give me all comments with commentable type Image and Room
+    $result = Comment::whereHasMorph(
+        'commentable',
+        ['App\Models\Image', 'App\Models\Room'],
+    )->get();
+
+
     // select * from `comments` where 
-    // (`comments`.`user_id` = 1 and `comments`.`user_id` is not null and `rating` > 3 or `rating` < 2) 
-    // and `comments`.`deleted_at` is null    
-    $result = User::find(1) // find user with id 1
-                ->comments() // and return comments available in User model
-                ->where('rating', '>', 3) // and query comments, where comment rating > 3
-                ->orWhere('rating', '<', 2) // or < 2
-                ->get(); // and get it at the end
+    // ((`comments`.`commentable_type` = 'App\\Models\\Image' and exists (select * from `images` where `comments`.`commentable_id` = `images`.`id`)) or (`comments`.`commentable_type` = 'App\\Models\\Room' and exists (select * from `rooms` where `comments`.`commentable_id` = `rooms`.`id`))) and `comments`.`deleted_at` is null
+    $result = Comment::whereHasMorph(
+        'commentable',
+        ['App\Models\Image', 'App\Models\Room'],
+        function ($query, $type) {
+            if ($type === 'App\Room')
+            {
+                $query->where('room_size', '>', 2);
+                $query->orWhere('room_size', '<', 2);
+            }
+            if ($type === 'App\Image')
+            {
+                $query->where('path', 'like', '%lorem%');
+            }        
+        }
+    )->get();
 
-    // select * from `users` where `users`.`id` = 1 limit 1
-    // select * from `comments` where `comments`.`user_id` = 1 and `comments`.`user_id` is not null and 
-    // (`rating` > 3 or `rating` < 2) 
-    // and `comments`.`deleted_at` is null                
-    $result = User::find(1)->comments()
-                ->where(function($query){ // this adds a pair of parentises / branckets into query
-                    return $query->where('rating', '>', 3)
-                            ->orWhere('rating', '<', 2);
-                })
-                ->get();
+    // select * from `comments` where `commentable_type` = 'App\\Models\\Room' and `rating` >= 3 and `comments`.`deleted_at` is null
+    // Get all Comments of Room where comment rating is at least 3
+    $result = Comment::where('commentable_type', Room::class)
+        ->where('rating', '>=', 3)
+        ->get();
 
-    // select * from `users` where 
-    // (select count(*) from `comments` 
-    // where `users`.`id` = `comments`.`user_id` and `comments`.`deleted_at` is null) >= 6
-    // give me user with at least 4 comments
-    $result = User::has('comments', '>=', 4)->get();
+
+    // Get all Comments of Room where comment rating is at least 3
+    // select * from `comments` where `commentable_type` = 'App\\Models\\Image' and `rating` >= 3 and `comments`.`deleted_at` is null
+    $result = Comment::where('commentable_type', Image::class)
+        ->where('rating', '>=', 3)
+        ->get();        
     
-    // select * from `comments` where exists 
-    // (select * from `users` where `comments`.`user_id` = `users`.`id` 
-    // and exists (select * from `addresses` where `users`.`id` = `addresses`.`user_id`)) 
-    // and `comments`.`deleted_at` is null;
-    // give me comments where user of that comment has address
-    $result = Comment::has('user.address')->get();
-
-    // select * from `users` where exists 
-    // (select * from `comments` where `users`.`id` = `comments`.`user_id` and `rating` > 2 and `comments`.`deleted_at` is null)
-    // give me user where it has comments with rating bigger then 2
-    $result = User::whereHas('comments', function ($query) {
-        $query->where('rating', '>', 2);
-    })
-    ->get();
-
-
-    // select * from `users` where (select count(*) from `comments` where `users`.`id` = `comments`.`user_id` and `rating` > 1 and `comments`.`deleted_at` is null) >= 2
-    // give me user where it has at least 2 comments with rating bigger then 1
-    $result = User::whereHas('comments', function ($query) {
-        $query->where('rating', '>', 1);
-    }, '>=', 2)
-    ->get();
-
+    // Get commentable of Image for comment id = 1
+    //select * from `comments` where `commentable_type` = 'App\\Models\\Image' and `comments`.`id` = 1 and `comments`.`deleted_at` is null limit 1
+    // $result = Comment::where('commentable_type', Image::class)->find(1);
     
-    // select * from `users` where not exists 
-    // (select * from `comments` where `users`.`id` = `comments`.`user_id` and `comments`.`deleted_at` is null)
-    // Give me user with without comments
-    $result = User::doesntHave('comments')->get(); // ->orDoesntHave
-
-    // give me user who didnt write a comment with rating less then 2
-    $result = User::whereDoesntHave('comments', function ($query) {
-        $query->where('rating', '<', 2); // you can add where here
-    })->get(); // ->orWhereDoesntHave
-
-    // give me all reservations which dont have comments that are rated less then 2 stars
-    $result = Reservation::whereDoesntHave('user.comments', function ($query) {
-        $query->where('rating', '<', 2);
-    })->get(); 
-
-    // select `users`.*, 
-    // (select count(*) from `comments` 
-    // where `users`.`id` = `comments`.`user_id` and `comments`.`deleted_at` is null) as `comments_count` from `users`;
-    // give me users with comments count
-    $result = User::withCount('comments')->get();
-
-
-    // select `users`.*, (select count(*) from `comments` where `users`.`id` = `comments`.`user_id` and `comments`.`deleted_at` is null) as `comments_count`, (select count(*) from `comments` where `users`.`id` = `comments`.`user_id` and `rating` <= 2 and `comments`.`deleted_at` is null) as `negative_comments_count` from `users`
-    // give me all Users with  comments count with negative rating (rating less  than 3)
-    $result = User::withCount([
-        'comments',
-        'comments as negative_comments_count' => function ($query) {
-            $query->where('rating', '<', 3);
-        },
-    ])->get();
+    // Comments with comentable of Image for all Comments Alternative: Using whereHasMorph (ensures the related Image exists)
+    // select * from `comments` where ((`comments`.`commentable_type` = 'App\\Models\\Image' and exists (select * from `images` where `comments`.`commentable_id` = `images`.`id`))) and `comments`.`deleted_at` is null
+    $result = Comment::whereHasMorph('commentable', Image::class)->get();
           
-    // dump($result[0]->comments_count,$result[0]->negative_comments_count);
+        
+dump($result);
 
 
-    dump($result);
+
     
     // return response()->json([
     //     'status' => 200,
